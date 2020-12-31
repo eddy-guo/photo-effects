94302b71e05be0aa4ea7f0d4f8e9554164161003
Eddy Guo <eddyyanruguo@gmail.com>
2020-12-30 23:01:26 -0500

    Add rbg function

--- script.js
+++ script.js
@@ -18,44 +18,57 @@ function uploadImage(e) {
   };
 }
 
-function greyscale () {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data
-    for (let i=0; i<data.length; i+=4) {
-        const grey = data[i] * 0.21 + data[i + 1] * 0.71 + data[i + 2] * 0.07;
-        data[i] = grey;
-        data[i + 1] = grey;
-        data[i + 2] = grey;
-    }
-    ctx.putImageData(imageData, 0, 0);
+function greyscale() {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const grey = data[i] * 0.21 + data[i + 1] * 0.71 + data[i + 2] * 0.07;
+    data[i] = grey;
+    data[i + 1] = grey;
+    data[i + 2] = grey;
+  }
+  ctx.putImageData(imageData, 0, 0);
 }
 
-function sepia () {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data
-    for (let i=0; i<data.length; i+=4) {
-        const grey = data[i] * 0.21 + data[i + 1] * 0.71 + data[i + 2] * 0.07;
-        data[i] = grey + 95;
-        data[i + 1] = grey + 58;
-        data[i + 2] = grey;
-    }
-    ctx.putImageData(imageData, 0, 0);
+function sepia() {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const grey = data[i] * 0.21 + data[i + 1] * 0.71 + data[i + 2] * 0.07;
+    data[i] = grey + 95;
+    data[i + 1] = grey + 58;
+    data[i + 2] = grey;
+  }
+  ctx.putImageData(imageData, 0, 0);
 }
 
-function invert () {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data
-    for (let i=0; i<data.length; i+=4) {
-        data[i] = 255 - data[i];
-        data[i + 1] = 255 - data[i + 1];
-        data[i + 2] = 255 - data[i + 2];
-    }
-    ctx.putImageData(imageData, 0, 0);
+function invert() {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i];
+    data[i + 1] = 255 - data[i + 1];
+    data[i + 2] = 255 - data[i + 2];
+  }
+  ctx.putImageData(imageData, 0, 0);
 }
 
-document.querySelectorAll('button')[0].addEventListener('click', greyscale);
-document.querySelectorAll('button')[1].addEventListener('click', sepia);
-document.querySelectorAll('button')[2].addEventListener('click', invert);
+function rbg() {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const green = data[i + 1];
+    data[i] = data[i];
+    data[i + 1] = data[i + 2];
+    data[i + 2] = green;
+  }
+  ctx.putImageData(imageData, 0, 0);
+}
+
+document.querySelectorAll("button")[0].addEventListener("click", greyscale);
+document.querySelectorAll("button")[1].addEventListener("click", sepia);
+document.querySelectorAll("button")[2].addEventListener("click", invert);
+document.querySelectorAll("button")[3].addEventListener("click", rbg);
 
 const imageLoader = document.getElementById("uploader");
 imageLoader.addEventListener("change", uploadImage);
