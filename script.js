b040406914746f104303bfd738fb3a417e27bfea
Eddy Guo <eddyyanruguo@gmail.com>
2020-12-30 23:02:05 -0500

    Add bgr function

--- script.js
+++ script.js
@@ -65,10 +65,23 @@ function rbg() {
   ctx.putImageData(imageData, 0, 0);
 }
 
+function bgr() {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const red = data[i];
+    data[i] = data[i + 2];
+    data[i + 1] = data[i + 1];
+    data[i + 2] = red;
+  }
+  ctx.putImageData(imageData, 0, 0);
+}
+
 document.querySelectorAll("button")[0].addEventListener("click", greyscale);
 document.querySelectorAll("button")[1].addEventListener("click", sepia);
 document.querySelectorAll("button")[2].addEventListener("click", invert);
 document.querySelectorAll("button")[3].addEventListener("click", rbg);
+document.querySelectorAll("button")[4].addEventListener("click", bgr);
 
 const imageLoader = document.getElementById("uploader");
 imageLoader.addEventListener("change", uploadImage);
