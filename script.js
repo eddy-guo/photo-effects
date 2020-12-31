48f131e989c1a2d503e71c3ad7442dec28573f9c
Eddy Guo <eddyyanruguo@gmail.com>
2020-12-30 23:02:58 -0500

    Add grb function

--- script.js
+++ script.js
@@ -89,12 +89,25 @@ function gbr() {
   ctx.putImageData(imageData, 0, 0);
 }
 
+function grb() {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const red = data[i];
+    data[i] = data[i + 1];
+    data[i + 1] = red;
+    data[i + 2] = data[i + 2];
+  }
+  ctx.putImageData(imageData, 0, 0);
+}
+
 document.querySelectorAll("button")[0].addEventListener("click", greyscale);
 document.querySelectorAll("button")[1].addEventListener("click", sepia);
 document.querySelectorAll("button")[2].addEventListener("click", invert);
 document.querySelectorAll("button")[3].addEventListener("click", rbg);
 document.querySelectorAll("button")[4].addEventListener("click", bgr);
 document.querySelectorAll("button")[5].addEventListener("click", gbr);
+document.querySelectorAll("button")[6].addEventListener("click", grb);
 
 const imageLoader = document.getElementById("uploader");
 imageLoader.addEventListener("change", uploadImage);
