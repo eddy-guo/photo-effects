da3a5c43ffd802725a27db12f8960bb56b2fc05e
Eddy Guo <eddyyanruguo@gmail.com>
2020-12-30 22:51:42 -0500

    Add greyscale and sepia function

--- script.js
+++ script.js
@@ -18,18 +18,32 @@ function uploadImage(e) {
   };
 }
 
-function change () {
+function greyscale () {
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const data = imageData.data
     for (let i=0; i<data.length; i+=4) {
-        data[i];
-        data[i + 1];
-        data[i + 2];
+        const grey = data[i] * 0.21 + data[i + 1] * 0.71 + data[i + 2] * 0.07;
+        data[i] = grey;
+        data[i + 1] = grey;
+        data[i + 2] = grey;
     }
     ctx.putImageData(imageData, 0, 0);
 }
 
-document.querySelectorAll('button')[0].addEventListener('click', change);
+function sepia () {
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data
+    for (let i=0; i<data.length; i+=4) {
+        const grey = data[i] * 0.21 + data[i + 1] * 0.71 + data[i + 2] * 0.07;
+        data[i] = grey + 95;
+        data[i + 1] = grey + 58;
+        data[i + 2] = grey;
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
+
+document.querySelectorAll('button')[0].addEventListener('click', greyscale);
+document.querySelectorAll('button')[1].addEventListener('click', sepia);
 
 const imageLoader = document.getElementById("uploader");
 imageLoader.addEventListener("change", uploadImage);
